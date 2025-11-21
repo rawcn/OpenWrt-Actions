252f1495ba6d42d86768177a9f013aead025373e
HelloTime <hellotime166@gmail.com>
2025-11-22 00:13:55 +0800

    Remove commented-out git clone commands

--- rax3000me_immortalwrt-24/diy1.sh
+++ rax3000me_immortalwrt-24/diy1.sh
@@ -14,5 +14,3 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 自定义软件源
 git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
-# Easytier
-git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
